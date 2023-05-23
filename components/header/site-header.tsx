1213eefdc816791589d8d2c2f201b132a73a3257
Nam <namdeveloper.ca@gmail.com>
2023-05-23 23:06:04 +0700

    Responsive UI Mobile Nav

--- components/header/site-header.tsx
+++ components/header/site-header.tsx
@@ -1,3 +1,8 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import ModeToggle from './mode-toggle'
+import MobileMenu from './mobile-menu'
+
 export function SiteHeader() {
   return (
     <header className='relative z-50 flex flex-col pointer-events-none'>
@@ -10,16 +15,16 @@ export function SiteHeader() {
                 <div className='relative flex gap-4'>
                   <div className='flex flex-1'>
                     <div className='h-10 w-10 rounded-full bg-white/90 p-0.5 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10 '>
-                      <a href='/' aria-label='Home' className='pointer-events-auto'>
-                        <picture>
-                          <img
-                            src='./headshot-small.png'
-                            alt="Namm's headshot"
-                            className='object-cover rounded-full bg-zinc-100 dark:bg-zinc-800 h-9 w-9'
-                            decoding='async'
-                          />
-                        </picture>
-                      </a>
+                      <Link href='/' aria-label='Home' className='pointer-events-auto'>
+                        <Image
+                          width={36}
+                          height={36}
+                          src='/headshot-small.png'
+                          alt="Namm's headshot"
+                          className='object-cover rounded-full bg-zinc-100 dark:bg-zinc-800 h-9 w-9'
+                          decoding='async'
+                        />
+                      </Link>
                     </div>
                   </div>{' '}
                   <div className='flex justify-end flex-1 md:justify-center'>
@@ -46,6 +51,7 @@ export function SiteHeader() {
                           />
                         </svg>
                       </button>{' '}
+                      <MobileMenu />
                     </div>{' '}
                     <nav className='hidden pointer-events-auto md:block'>
                       <ul className='flex px-3 text-sm font-medium rounded-full shadow-lg bg-white/90 text-zinc-800 shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10'>
@@ -95,38 +101,7 @@ export function SiteHeader() {
                   </div>{' '}
                   <div className='flex justify-end md:flex-1'>
                     <div className='pointer-events-auto'>
-                      <button
-                        type='button'
-                        aria-label='Toggle dark mode'
-                        className='px-3 py-2 transition rounded-full shadow-lg group bg-white/90 shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20'
-                      >
-                        <svg
-                          viewBox='0 0 24 24'
-                          strokeWidth='1.5'
-                          strokeLinecap='round'
-                          strokeLinejoin='round'
-                          aria-hidden='true'
-                          className='h-6 w-6 fill-zinc-100 stroke-zinc-500 transition group-hover:fill-zinc-200 group-hover:stroke-zinc-700 dark:hidden [@media(prefers-color-scheme:dark)]:fill-teal-50 [@media(prefers-color-scheme:dark)]:stroke-teal-500 [@media(prefers-color-scheme:dark)]:group-hover:fill-teal-50 [@media(prefers-color-scheme:dark)]:group-hover:stroke-teal-600'
-                        >
-                          <path d='M8 12.25A4.25 4.25 0 0 1 12.25 8v0a4.25 4.25 0 0 1 4.25 4.25v0a4.25 4.25 0 0 1-4.25 4.25v0A4.25 4.25 0 0 1 8 12.25v0Z' />
-                          <path
-                            d='M12.25 3v1.5M21.5 12.25H20M18.791 18.791l-1.06-1.06M18.791 5.709l-1.06 1.06M12.25 20v1.5M4.5 12.25H3M6.77 6.77 5.709 5.709M6.77 17.73l-1.061 1.061'
-                            fill='none'
-                          />
-                        </svg>
-                        <svg
-                          viewBox='0 0 24 24'
-                          aria-hidden='true'
-                          className='hidden h-6 w-6 fill-zinc-700 stroke-zinc-500 transition dark:block [@media(prefers-color-scheme:dark)]:group-hover:stroke-zinc-400 [@media_not_(prefers-color-scheme:dark)]:fill-teal-400/10 [@media_not_(prefers-color-scheme:dark)]:stroke-teal-500'
-                        >
-                          <path
-                            d='M17.25 16.22a6.937 6.937 0 0 1-9.47-9.47 7.451 7.451 0 1 0 9.47 9.47ZM12.75 7C17 7 17 2.75 17 2.75S17 7 21.25 7C17 7 17 11.25 17 11.25S17 7 12.75 7Z'
-                            strokeWidth='1.5'
-                            strokeLinecap='round'
-                            strokeLinejoin='round'
-                          />
-                        </svg>
-                      </button>
+                      <ModeToggle />
                     </div>
                   </div>
                 </div>
