e199aec268aaee0f43314b840d025ac70ad6c355
Nam <namdeveloper.ca@gmail.com>
2023-05-23 15:46:23 +0700

    Blog page, Projects Page, About Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,7 +54,9 @@ const fontSans = FontSans({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en' className='dark' suppressHydrationWarning>
-      <body className={cn('flex h-full flex-col bg-background font-sans', fontSans.variable)}>
+      <body
+        className={cn('flex h-full flex-col bg-zinc-50 dark:bg-black font-sans', fontSans.variable)}
+      >
         <div className='fixed inset-0 flex justify-center sm:px-8'>
           <div className='flex w-full max-w-7xl lg:px-8'>
             <div className='w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20' />
